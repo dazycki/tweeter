c4ac5761d33e549ca62993f2fa79b39c7ab22bf3
Daryan Zycki <da.zycki@gmail.com>
2022-04-02 22:20:35 +0000

    Add character count to new tweet section.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-  // --- our code goes here ---
+  
+  $('textarea').on('input', function() {
+    const maxLen = 140;
+    const characters = $(this).val().length;
+    $(this).nextAll(".counter").text(maxLen - characters);
+    if (maxLen - characters < 0) {
+      $(this).nextAll(".counter").css("color", "red");
+    } else {
+      $(this).nextAll(".counter").css("color", "#545149");
+    }
+  });
+  
 });
