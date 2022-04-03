758c42ed199302bf4178baecec56cf0bbb3656fa
Daryan Zycki <da.zycki@gmail.com>
2022-04-03 22:33:16 +0000

    Update styling of character count and remove redundant code.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,10 +5,8 @@ $(document).ready(function() {
     const characters = $(this).val().length;
     $(this).nextAll(".counter").text(maxLen - characters);
     if (maxLen - characters < 0) {
-      $(this).nextAll(".counter").css("color", "red");
-    } else {
-      $(this).nextAll(".counter").css("color", "#545149");
-    }
+      $(this).nextAll(".counter").css("color", "#810303");
+    } 
   });
   
 });
