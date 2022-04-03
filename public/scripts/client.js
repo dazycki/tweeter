876d4115e0038e1dbb766dc96117594dee180969
Daryan Zycki <da.zycki@gmail.com>
2022-04-03 05:58:26 +0000

    Add function to load tweets from /tweets.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,43 +1,17 @@
 $(document).ready(function() {
 
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+  const renderTweets = function(tweets) {
+    const $tweets = $('#tweets-container');
+    $tweets.empty();
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $tweets.prepend($tweet);
+    }
+  };
 
-const renderTweets = function(tweets) {
-  const $tweets = $('#tweets-container');
-  $tweets.empty();
-  for(const tweet of tweets) {
-    const $tweet = createTweetElement(tweet);
-    $tweets.prepend($tweet);
-  }
-}
-
-const createTweetElement = function(tweet) {
+  const createTweetElement = function(tweet) {
   
-  const $tweet = $('<article class="tweet">').append(`
+    const $tweet = $('<article class="tweet">').append(`
   <header>
     <span>
       <img src="${tweet.user.avatars}" class="avatar">
@@ -56,22 +30,34 @@ const createTweetElement = function(tweet) {
     </span>
   </footer>`);
 
-  return $tweet;
-}
+    return $tweet;
+  };
 
-renderTweets(data);
-
-const $form = $('#newTweetForm');
+  const $form = $('#newTweetForm');
   
-    $form.on('submit', function(event){
-      event.preventDefault();
-      const serializedData = $(event.target).serialize();
+  $form.on('submit', function(event) {
+    event.preventDefault();
+    const serializedData = $(event.target).serialize();
     
-      $.post('/tweets', serializedData, response => {
-        console.log(response)
-        renderTweets();
-      })
+    $.post('/tweets', serializedData, response => {
+      console.log(response);
+      renderTweets();
+    });
   
-    })
+  });
 
+  const loadTweets = (url, method, cb) => {
+    $.ajax({
+      url,
+      method,
+    })
+      .done(data => {
+        cb(data);
+      })
+      .fail(err => {
+        console.log('Error:', err);
+      })
+  };
+
+  loadTweets("/tweets", "GET", renderTweets);
 });
